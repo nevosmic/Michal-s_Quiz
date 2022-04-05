56480294a50e6624948bca23bbc79863323da02a
nevosmic <nevosmic@post.bgu.ac.il>
2022-04-05 15:48:25 +0300

    moving to the next question

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,37 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "./Questions.css";
-
-/*const questionss = [
-  {
-    category: "Entertainment: Board Games",
-    type: "multiple",
-    difficulty: "easy",
-    question:
-      "In a standard game of Monopoly, what colour are the two cheapest properties?",
-    correct_answer: "Brown",
-    incorrect_answers: ["Green", "Yellow", "Blue"],
-  },
-  {
-    category: "Science & Nature",
-    type: "multiple",
-    difficulty: "easy",
-    question:
-      "What does the letter &#039;S&#039; stand for in &#039;NASA&#039;?",
-    correct_answer: "Space",
-    incorrect_answers: ["Science", "Society", "Star"],
-  },
-];*/
+import Card from "../UI/Card";
+//TODO  shuffledAnswers
+/*I used dangerouslySetInnerHTML to fix the quotes gibberish problem */
 
 const questionsAPI = "https://opentdb.com/api.php?amount=100";
 
-//const shuffledAnswers =
+//const Button = (answer) =>{}
 
 const Questions = () => {
-  /*const variables */
+  /*state variables */
   const [currentScore, setCurrentScore] = useState(0);
-
-  //const [currentQuestion, setCurrentQuestion] = useState();
+  const [currentQuestion, setCurrentQuestion] = useState(0);
   const [questionsFromAPI, setQuestionsFromAPI] = useState([]);
+
+  /*Helper functions*/
 
   const fetchData = () => {
     return fetch(questionsAPI)
@@ -43,39 +26,50 @@ const Questions = () => {
     fetchData();
   }, []);
 
-  /*Helper functions*/
   const clickHandler = (isCorrect) => {
+    /* Correct answer : 
+    -update score 
+    -move to the next question*/
     console.log(isCorrect);
-    /*update score if correct*/
     if (isCorrect) {
       setCurrentScore((prevState) => {
         return prevState + 1;
       });
+      setCurrentQuestion((prevState) => {
+        return prevState + 1;
+      });
     }
   };
+
+  /*rendering*/
   if (questionsFromAPI.length > 0) {
     return (
       <div className="Questions">
         <h2>Questions</h2>
 
-        <div className="question-card">
+        <Card>
           <h2>current score: {currentScore}</h2>
-          <h3 className="question-text">{questionsFromAPI[0].question}</h3>
+          <h3
+            className="question-text"
+            dangerouslySetInnerHTML={{
+              __html: questionsFromAPI[currentQuestion].question,
+            }}
+          />
           <ul>
             <li onClick={() => clickHandler(true)}>
-              {questionsFromAPI[0].correct_answer}
+              {questionsFromAPI[currentQuestion].correct_answer}
             </li>
             <li onClick={() => clickHandler(false)}>
-              {questionsFromAPI[0].incorrect_answers[0]}
+              {questionsFromAPI[currentQuestion].incorrect_answers[0]}
             </li>
             <li onClick={() => clickHandler(false)}>
-              {questionsFromAPI[0].incorrect_answers[1]}
+              {questionsFromAPI[currentQuestion].incorrect_answers[1]}
             </li>
             <li onClick={() => clickHandler(false)}>
-              {questionsFromAPI[0].incorrect_answers[2]}
+              {questionsFromAPI[currentQuestion].incorrect_answers[2]}
             </li>
           </ul>
-        </div>
+        </Card>
       </div>
     );
   } else {
